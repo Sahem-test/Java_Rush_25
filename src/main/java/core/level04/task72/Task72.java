3f489492bb1ac1f924133a18950a514f8849b6a5
Admin <dir.gefest@gmail.com>
2026-03-08 10:11:17 +0300

    Task72 -73

--- src/main/java/core/level04/task72/Task72.java
+++ src/main/java/core/level04/task72/Task72.java
@@ -15,7 +15,7 @@ package main.java.core.level04.task72;
 public class Task72 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
+            for (int j = 1; j <=  10; j++) {
                 if ((i+j)==13)
                 {
                     System.out.println("Пароль найден: i=" + i + ", j=" + j);
