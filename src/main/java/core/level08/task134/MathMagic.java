9575e03f27a67955bd62a3fcab8c9f61f399a67f
Sahem-test <testroj76@gmail.com>
2026-03-27 08:41:03 +0300

    Task134

---
+++ src/main/java/core/level08/task134/MathMagic.java
@@ -0,0 +1,9 @@
+package main.java.core.level08.task134;
+
+public class MathMagic {
+
+    public static void showSum(int firstNumber, int secondNumber){
+        int sum = firstNumber+secondNumber;
+        System.out.println(sum);
+    }
+}
