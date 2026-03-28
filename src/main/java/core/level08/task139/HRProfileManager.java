bba63c0117733af9152b7a5176ff38fecbb5407d
Sahem-test <testroj76@gmail.com>
2026-03-28 09:27:52 +0300

    Task139

---
+++ src/main/java/core/level08/task139/HRProfileManager.java
@@ -0,0 +1,9 @@
+package main.java.core.level08.task139;
+
+public class HRProfileManager {
+
+    public static void displayEmployeeCard(String employeeName, int employeeAge) {
+        String result = "Имя сотрудника: " + employeeName + ", " + "Возраст: " + employeeAge;
+        System.out.println(result);
+    }
+}
