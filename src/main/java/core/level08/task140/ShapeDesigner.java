1bed8a0ee2407c07ed82c8166228698e103dae0f
Sahem-test <testroj76@gmail.com>
2026-03-28 09:35:40 +0300

    Task140

---
+++ src/main/java/core/level08/task140/ShapeDesigner.java
@@ -0,0 +1,9 @@
+package main.java.core.level08.task140;
+
+public class ShapeDesigner {
+
+    public static void printRectangleSpecs(int rectangleWidth, int rectangleHeight) {
+        String result = "Характеристики прямоугольника: ширина = " + rectangleWidth + ", " + "высота = " + rectangleHeight;
+        System.out.println(result);
+    }
+}
