c7f3bdce4f831d68368868b6db1c7dcc1544d81c
Sahem-test <testroj76@gmail.com>
2026-03-28 09:20:17 +0300

    Task138

---
+++ src/main/java/core/level08/task138/StoreCashier.java
@@ -0,0 +1,10 @@
+package main.java.core.level08.task138;
+
+public class StoreCashier {
+
+
+    public static void calculateAndShowCost(int firstItemPrice, int secondItemPrice){
+        int result = firstItemPrice + secondItemPrice;
+        System.out.println("Общая стоимость покупки: " + result);
+    }
+}
