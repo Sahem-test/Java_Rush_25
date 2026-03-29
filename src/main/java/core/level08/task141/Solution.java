0886472cfee403cb87af578ea0d40cb959be8f0d
Sahem-test <testroj76@gmail.com>
2026-03-29 08:27:44 +0300

    Task141

---
+++ src/main/java/core/level08/task141/Solution.java
@@ -0,0 +1,8 @@
+package main.java.core.level08.task141;
+
+public class Solution {
+
+    public static String generateGreetingMessage(String recipientName) {
+        return "Привет, " + recipientName + "!";
+    }
+}
