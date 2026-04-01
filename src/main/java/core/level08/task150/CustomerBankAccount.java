e14b012ae8305a74d030b8a90ae7bfe39ebf76e1
Sahem-test <testroj76@gmail.com>
2026-04-01 08:31:26 +0300

    Task150

---
+++ src/main/java/core/level08/task150/CustomerBankAccount.java
@@ -0,0 +1,9 @@
+package main.java.core.level08.task150;
+
+public class CustomerBankAccount {
+    private int accountBalance=0;
+
+  public void depositMoney(int amountToDeposit){
+      accountBalance+=amountToDeposit;
+  }
+}
