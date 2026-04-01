19ba27735730f17c8e58d9fd2d89c97fc50d9cb0
Sahem-test <testroj76@gmail.com>
2026-04-01 08:38:35 +0300

    Task150

---
+++ src/main/java/core/level08/task151/FactoryBox.java
@@ -0,0 +1,9 @@
+package main.java.core.level08.task151;
+
+public class FactoryBox {
+    public int boxSize;
+
+    public void setDimensions(int boxSize){
+        this.boxSize = boxSize;
+    }
+}
